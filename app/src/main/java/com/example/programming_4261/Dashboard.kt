8c7dc553081f7752d6ffb0f810ef37ebe25a9863
kazilfr <kazilfr@gmail.com>
2024-01-28 19:09:13 -0500

    Add Basic Dashboard Screen

--- app/src/main/java/com/example/programming_4261/Dashboard.kt
+++ app/src/main/java/com/example/programming_4261/Dashboard.kt
@@ -2,10 +2,16 @@ package com.example.programming_4261
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class Dashboard : AppCompatActivity() {
+    lateinit var user: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        user = intent.getStringExtra("user").toString()
+        val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
+        tvGreeting.text = "Hello: $user"
     }
 }
