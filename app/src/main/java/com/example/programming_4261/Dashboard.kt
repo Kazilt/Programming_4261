2acd3fd6a061aece982623387af5b40073f8d15b
kazilfr <kazilfr@gmail.com>
2024-01-28 19:37:47 -0500

    Add User count to dashboard

--- app/src/main/java/com/example/programming_4261/Dashboard.kt
+++ app/src/main/java/com/example/programming_4261/Dashboard.kt
@@ -2,8 +2,13 @@ package com.example.programming_4261
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.firestore
 
 class Dashboard : AppCompatActivity() {
     lateinit var user: String
@@ -12,6 +17,18 @@ class Dashboard : AppCompatActivity() {
         setContentView(R.layout.activity_dashboard)
         user = intent.getStringExtra("user").toString()
         val tvGreeting = findViewById<TextView>(R.id.tvGreeting)
+        val numUsers = findViewById<TextView>(R.id.tvNumUsers)
+        val users = Firebase.firestore.collection("users")
+
         tvGreeting.text = "Hello: $user"
+        countUsers(numUsers, users)
+
+    }
+    private fun countUsers(numUsers: TextView, users: CollectionReference) {
+        users.get().addOnSuccessListener { documents ->
+            numUsers.text = "Total Users: ${documents.size()}"
+            }.addOnFailureListener {exception ->
+                Log.w("IMP", "Error getting documents: ", exception)
+            }
     }
 }
